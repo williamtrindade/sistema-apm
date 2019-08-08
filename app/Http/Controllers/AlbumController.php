9166a1b76791c6072741589f1a6f90a4a736c487
william <williamtrindade777@gmail.com>
2019-08-08 11:11:11 -0300

    update

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -17,7 +17,7 @@ class AlbumController extends Controller
     public function index()
     {
         return view('albums.index')
-            ->with('albums', Album::where('nivel', 0)->orderBy('created_at', 'DESC')->get());
+            ->with('albums', Album::where('nivel', 0)->orderBy('created_at', 'DESC')->paginate('10'));
     }
 
     /**
